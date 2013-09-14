49a0fe88bdd1b670702322734149bc4589071b6f
EvanSimpson <esimpson.rescue@gmail.com>
2013-09-13 23:13:44 -0700

    UI update and realtime text replace

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -1,4 +1,31 @@
+function repeat (str, n) {
+	return Array(n + 1).join('\n').split('').map(function () { return str; }).join("");
+}
+
+function censor (words, input, callback) {
+	words.forEach(function (w) {
+		var re = new RegExp("\\b"+w+"\\b", 'gi');
+		input = input.replace(re, repeat("█", w.length));
+	});
+	callback(input);
+}
+
+
+var words;
+
 $( document ).ready(function() {
+
+	$.get('/api/list', function(res){
+		words = res.list;
+	});
+
+
+	$("#styled").keyup( function(){
+		console.log(words);
+		censor(words, $("#styled").val(), function(newText){
+			$("#styled").val(newText);
+		});
+	});
 
 	$("#button").click(function(){
 		$.post( '/api/convert', {text:$("#textarea").val()} , function(res){
@@ -14,6 +41,9 @@ $( document ).ready(function() {
 				$("#main").fadeIn(400);
 			});
 	});
+
+
+	
 
 
 });
