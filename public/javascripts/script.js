f4c3d334aacb90a3ecea1efcec4a2fd32f5eaef5
EvanSimpson <esimpson.rescue@gmail.com>
2013-09-14 13:08:37 -0700

    Adding auto typer

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -13,6 +13,19 @@ function censor (words, input, callback) {
 	callback(input.replace(/\n/g, "</br>"));
 };
 
+var tl=new Array(
+"Welcome! ",
+"Our goal is to inform everyone about censorship from the NSA. ",
+"Make sure you have a secure internet connection, ",
+"otherwise watch for the CIA white van outside. ",
+"If you have an idea to promote internet privacy or freedom ",
+"let us know!"
+);
+var speed=60;
+var index=0; text_pos=0;
+var str_length=tl[0].length;
+var contents, row;
+var clicked = false;
 
 
 $( document ).ready(function() {
@@ -22,6 +35,7 @@ $( document ).ready(function() {
 	});
 
 	$("#styled").click(function(){
+		clicked = true;
 		$("#styled").attr('data-placeholder', "");
 	});
 
@@ -36,6 +50,36 @@ $( document ).ready(function() {
 		});
     });
 
+    function type_text(){
+    	if(!clicked){
+		  $("#styled").attr('data-placeholder', "");
+		  contents='';
+		  row=Math.max(0,index-7);
+		  while(row<index)
+		    contents += tl[row++];
+		  $("#styled").html(contents + tl[index].substring(0,text_pos) + "_");
+		  $("#parent").height($("#styled")[0].scrollHeight + 20);
+			var input = $("#styled").clone().children().remove().end().text();
+			$("#styled").children().each(function(){
+				input = input + "\n" + $(this).text();
+			});
+			censor(words, input, function(newText){
+			$("#underlay").html(newText);
+			});
+		  if(text_pos++==str_length)
+		  {
+		    text_pos=0;
+		    index++;
+		    if(index!=tl.length)
+		    {
+		      str_length=tl[index].length;
+		      setTimeout(type_text,1500);
+		    }
+		  } else{
+		      setTimeout(type_text,speed);
+		  }
+		}
+	}
 
 	$("#button").click(function(){
 		$.post( '/api/convert', {text:$("#textarea").val()} , function(res){
@@ -52,5 +96,7 @@ $( document ).ready(function() {
 				$("#main").fadeIn(400);
 			});
 	});
+
+	setTimeout(type_text, 5000);
 
 });
