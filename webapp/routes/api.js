8903cf3618d80230fe1bf25518e2135a5494531c
EvanSimpson <esimpson.rescue@gmail.com>
2013-09-13 20:56:44 -0700

    Updating webonly changes to master

--- webapp/routes/api.js
+++ webapp/routes/api.js
@@ -1,13 +1,17 @@
 var fs = require('fs');
 
+function repeat (str, n) {
+	return Array(n + 1).join('\n').split('').map(function () { return str; }).join("");
+}
+
 var words = fs.readFileSync("blacklist.txt", "utf-8").split(/\n/);
 
 exports.convert = function(req, res){
 	var input = req.body.text;
-	words.some(function (w) {
+	words.forEach(function (w) {
 		var re = new RegExp("\\b"+w+"\\b", 'g');
 		console.log(re);
-		input = input.replace(re, '[redacted]');
+		input = input.replace(re, repeat("█", w.length));
 	});
 	res.send(input);
 }
