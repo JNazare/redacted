8fddb2c016c750e76f1ddbc4c3584f61a5a5b74e
EvanSimpson <esimpson.rescue@gmail.com>
2013-09-13 20:35:24 -0700

    Changing api call

--- routes/api.js
+++ routes/api.js
@@ -4,7 +4,7 @@ var words = fs.readFileSync("blacklist.txt", "utf-8").split(/\n/);
 
 exports.convert = function(req, res){
 	var input = req.body.text;
-	words.some(function (w) {
+	words.forEach(function (w) {
 		var re = new RegExp("\\b"+w+"\\b", 'g');
 		console.log(re);
 		input = input.replace(re, '[redacted]');
